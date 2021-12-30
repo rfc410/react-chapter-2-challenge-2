bf4e8ee966d307ddf5b054e9b7a5c29563b68dd9
Caio Rodrigues <rfcaio90@gmail.com>
2021-12-30 12:03:27 -0300

    add challenge solution

--- src/components/Food/styles.ts
+++ src/components/Food/styles.ts
@@ -1,6 +1,10 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from 'styled-components'
 
-export const Container = styled.div`
+interface ContainerProps {
+  available: boolean
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #f0f0f5;
   border-radius: 8px;
 
@@ -142,4 +146,4 @@ export const Container = styled.div`
       }
     }
   }
-`;
+`
